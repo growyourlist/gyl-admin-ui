f89261a6f898333e761f89a6ac84d72e2e00a1d8
gyl-dev <gyl-dev@posteo.eu>
2019-11-12 13:23:16 +0100

    Improve login UX

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import './style.scss'
 import { hsh } from './common/hsh'
+import { showSiteError } from './common/showSiteError'
 
-const { byId, firstBySelector, onDOMReady } = hsh
+const { byId, firstBySelector, onDOMReady, newElm } = hsh
 
 onDOMReady(() => {
     const signInButton = byId('sign-in-button')
     signInButton.aEL('click', () => {
         const apiUrl = byId('gyl-api-url')
         const apiKey = byId('gyl-api-key')
-        sessionStorage.setItem('gyl-api-url', apiUrl.value)
+        if (!apiUrl.value || !apiKey.value) {
+            return showSiteError(newElm(
+                'p', 'Please enter both the API URL and API key.'
+            ))
+        }
+        const apiUrlString = (apiUrl.value.slice(-1) === '/') ?
+            apiUrl.value.substring(0, apiUrl.value.length - 1) :
+            apiUrl.value
+        sessionStorage.setItem('gyl-api-url', apiUrlString)
         sessionStorage.setItem('gyl-api-key', apiKey.value)
+        const apiConnectionContainer = firstBySelector('.api-connection-container')
+        apiConnectionContainer.style.display = 'none'
         const adminScreenMenu = firstBySelector('.admin-menu')
-        signInButton.style.display = 'none'
         adminScreenMenu.style.display = null
     })
 })
