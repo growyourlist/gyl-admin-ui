5f5dcbaad5866602613940c43ec88e08ce55fd8a
gyl-dev <gyl-dev@posteo.eu>
2019-12-06 23:55:30 +0100

    Inline scripts first attempt

--- webpack.config.js
+++ webpack.config.js
@@ -6,36 +6,43 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			filename: 'index.html',
+			inject: 'body',
 			template: './src/index.pug',
 			chunks: ['gylBase', 'index'],
 		}),
 		new HtmlWebpackPlugin({
 			filename: 'broadcast/index.html',
+			inject: 'body',
 			template: './src/broadcast/broadcast.pug',
 			chunks: ['gylBase', 'broadcast'],
 		}),
 		new HtmlWebpackPlugin({
 			filename: 'emails/index.html',
+			inject: 'body',
 			template: './src/emails/emails.pug',
 			chunks: ['gylBase', 'emails'],
 		}),
 		new HtmlWebpackPlugin({
 			filename: 'autoresponders/index.html',
+			inject: 'body',
 			template: './src/autoresponders/autoresponders.pug',
 			chunks: ['gylBase', 'autoresponders'],
 		}),
 		new HtmlWebpackPlugin({
 			filename: 'lists/index.html',
+			inject: 'body',
 			template: './src/lists/lists.pug',
 			chunks: ['gylBase', 'lists'],
 		}),
 		new HtmlWebpackPlugin({
 			filename: 'analytics/index.html',
+			inject: 'body',
 			template: './src/analytics/analytics.pug',
 			chunks: ['gylBase', 'analytics'],
 		}),
 		new HtmlWebpackPlugin({
 			filename: 'export-data/index.html',
+			inject: 'body',
 			template: './src/export-data/export-data.pug',
 			chunks: ['gylBase', 'exportData'],
 		})
