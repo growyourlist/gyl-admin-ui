aa9152cc183688d7ebe792740815c111364db529
gyl-dev <gyl-dev@posteo.eu>
2020-03-27 18:25:10 +0100

    Remove generate data script

--- generate-test-data.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-let content = '';
-for (let i = 0; i < 200; i++) {
-	content += `"test${i}@example.com","Test",1577836800000,"taga,tagb"\n`;
-}
-fs.writeFileSync('testdata.csv', content);
